05c0b7418f9fb210a665a9b9054616a7403950fa
Sergio Pascual <sergiopr@fis.ucm.es>
2024-01-21 13:53:13 +0100

    Add missing routines in plplot

---
+++ src/extras.f90
@@ -0,0 +1,69 @@
+
+subroutine pgsls(ls)
+  implicit none
+  integer, intent(in) :: ls
+  
+end subroutine pgsls
+
+subroutine plmove
+  
+end subroutine plmove
+
+
+
+subroutine pldraw
+  
+end subroutine pldraw
+
+subroutine plsls
+  
+end subroutine plsls
+
+subroutine plsch
+  
+end subroutine plsch
+!
+subroutine plask
+  
+end subroutine plask
+!
+subroutine plptext
+  
+end subroutine plptext
+!
+subroutine plpoint
+  
+end subroutine plpoint
+
+!
+subroutine plend
+  
+end subroutine plend
+!
+
+subroutine plbegin
+  
+end subroutine plbegin
+subroutine plpage
+  
+end subroutine plpage
+
+subroutine plvport
+  
+end subroutine plvport
+
+subroutine plwindow
+  
+end subroutine plwindow
+
+subroutine plsfs
+  
+end subroutine plsfs
+
+subroutine plrect
+  
+end subroutine plrect
+
+subroutine plscf
+  
+end subroutine plscf
